9dc2fe375406334014cef4a42875c892580ea864
alexoliveiramartins <alex.oliveiramartins05@gmail.com>
2025-02-09 22:27:56 -0300

    Initial Commit

---
+++ src/Task.java
@@ -0,0 +1,69 @@
+import java.util.Date;
+
+public class Task {
+
+    private String name;
+    private String description;
+    private Date dueDate;
+    private int priority;
+    private String category;
+    private String status;
+
+    public Task(String name, String description, Date dueDate, int priority, String category, String status) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.category = category;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
